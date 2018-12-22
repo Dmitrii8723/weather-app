6823733899b8f8764ebb0f2ab1a9299fa3808e3b
dud <dimakharapaev@yandex.ru>
2018-12-22 12:30:02 +1100

    Format the code

--- index.js
+++ index.js
@@ -10,33 +10,32 @@ app.get('/api/weather', (req, res) => {
     const lat = req.query.lat;
     const lon = req.query.lon;
     const apiKey = req.query.apiKey;
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-        console.log('apiKey:', apiKey);
-        let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        request(url, function (err, response, body) {
-            if (err) {
-                console.log('error:', error);
-            } else {
-                res.send(body);
-                item = {
-                    body: body
-                };
-                console.log('body:', body);
-            }
-        });
+    request(url, function (err, response, body) {
+        if (err) {
+            console.log('error:', error);
+        } else {
+            res.send(body);
+            item = {
+                body: body
+            };
+            console.log('body:', body);
+        }
+    });
 
 //Creating a collection and storing a data
-        const dbName = 'test';
-        MongoClient.connect(urlMongo, function (err, client) {
-            assert.equal(null, err);
-            console.log('It is connected');
-            const db = client.db(dbName);
-            db.collection('testcollection').insertOne(item, function (err, result) {
-                assert.equal(1, result.insertedCount);
-                console.log('It is stored');
-                client.close();
-            });
+    const dbName = 'test';
+    MongoClient.connect(urlMongo, function (err, client) {
+        assert.equal(null, err);
+        console.log('It is connected');
+        const db = client.db(dbName);
+        db.collection('testcollection').insertOne(item, function (err, result) {
+            assert.equal(1, result.insertedCount);
+            console.log('It is stored');
+            client.close();
         });
+    });
 });
 
 app.listen(3000, () => console.log('Listening on port 3000....'));
