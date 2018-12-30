219e9303ee01be02d1966361c01108df6fd7e4c2
dud <dimakharapaev@yandex.ru>
2018-12-30 20:52:19 +1100

    Add a distance calculation functionality

--- README.txt
+++ README.txt
@@ -14,3 +14,16 @@ http://localhost:3000
 
 5) Use this date format "mm/dd/yy" in order to access a historical data
 
+6) Use a transponder of an aircraft from a receivded "Aircraft" data (This data can be received by entering coordinates of a particluar location (usually an Airport)). 
+
+Example of a received data: 
+
+["ab5d52","DAL469 ","United States",1546162049,1546162049,-73.1382,40.5666,3467.1,false,126.27,229.63,-11.38,null,3444.24,"2404",false,0]
+
+from this example the first element of an array is a transponder, in this case it's "ab5d52". Also, "-73.1382" and "40.5666" are coordinates of 
+
+an aircraft at the moment when this data was received from an open api 
+
+Enter this transponder into "Transponder" field in order to see a distance that an aircraft travelled from the time when the data was retrieved till
+
+the moment when the distance calculation is requested. Also, this distance can be recalculated as many times as you would like.
--- index.js
+++ index.js
@@ -20,7 +20,13 @@ math.degrees = (radians) => {
 
 app.engine('html', require('ejs').renderFile);
 app.get('/', (req, res) => {
-    res.render('index.html', {items: null, itemsfiltered: null, itemsaircraft: null, itemsaircraftfiltered: null});
+    res.render('index.html', {
+        items: null,
+        itemsfiltered: null,
+        itemsaircraft: null,
+        itemsaircraftfiltered: null,
+        itemstrack: null
+    });
 });
 
 app.get('/api/weather', (req, res) => {
@@ -32,14 +38,14 @@ app.get('/api/weather', (req, res) => {
     let dbName = 'test';
     MongoClient.connect(urlMongo, (err, client) => {
         db = client.db(dbName);
-        if (!timestamp) {
+        if (lat && lon) {
             const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
             request(url, (err, response, body) => {
                 if (err) {
                     console.log('error:', error);
                 } else {
                     item = {
-                        body: body
+                        body: JSON.parse(body)
                     };
                     console.log('body:', item);
                 }
@@ -57,12 +63,13 @@ app.get('/api/weather', (req, res) => {
                         items: body,
                         itemsaircraft: null,
                         itemsfiltered: null,
-                        itemsaircraftfiltered: null
+                        itemsaircraftfiltered: null,
+                        itemstrack: null
                     });
                 });
             });
 
-        } else {
+        } else if (timestamp) {
             let resultArray = [];
             let cursor = db.collection('weather').find({timestamp: {$eq: timestamp}});
             cursor.forEach((doc) => {
@@ -75,15 +82,23 @@ app.get('/api/weather', (req, res) => {
                     itemsfiltered: JSON.stringify(resultArray),
                     items: null,
                     itemsaircraft: null,
-                    itemsaircraftfiltered: null
+                    itemsaircraftfiltered: null,
+                    itemstrack: null
                 });
+            });
+        } else {
+            res.render('index.html', {
+                itemsfiltered: null,
+                items: null,
+                itemsaircraft: null,
+                itemsaircraftfiltered: null,
+                itemstrack: null
             });
         }
     });
 });
 
 app.get('/api/aircraft', (req, res) => {
-
     const latitudemin = parseInt(req.query.lat) - 1;
     const longitudemin = parseInt(req.query.lon) - 1;
     const latitudemax = parseInt(req.query.lat) + 1;
@@ -93,23 +108,24 @@ app.get('/api/aircraft', (req, res) => {
 
     MongoClient.connect(urlMongo, (err, client) => {
         db = client.db(dbName);
-        if (!timestamp) {
-            const urlFlight = `https://opensky-network.org/api/states/all?lamin=${latitudemin}&lomin=${longitudemin}&lamax=${latitudemax}&lomax=${longitudemax}`;
+        const urlFlight = `https://opensky-network.org/api/states/all?lamin=${latitudemin}&lomin=${longitudemin}&lamax=${latitudemax}&lomax=${longitudemax}`;
+        console.log('urlFlight: ', urlFlight);
+        if (urlFlight !== "https://opensky-network.org/api/states/all?lamin=NaN&lomin=NaN&lamax=NaN&lomax=NaN") {
             request(urlFlight, (err, response, body) => {
                 if (err) {
                     console.log('error:', error);
                 } else {
-                    item = {
-                        body: body
+                    itemsaircaftrack = {
+                        body: JSON.parse(body)
                     };
-                    console.log('item:', body);
+                    console.log('item:', itemsaircaftrack.body.states);
                 }
                 assert.equal(null, err);
                 console.log('It is connected');
 
                 let currentdate = new Date();
                 db.collection('aircraft').insertOne({
-                    item,
+                    itemsaircaftrack,
                     timestamp: dateFormat(currentdate, "mm/dd/yy")
                 }, (err, result) => {
                     assert.equal(1, result.insertedCount);
@@ -119,11 +135,12 @@ app.get('/api/aircraft', (req, res) => {
                         itemsaircraft: body,
                         items: null,
                         itemsfiltered: null,
-                        itemsaircraftfiltered: null
+                        itemsaircraftfiltered: null,
+                        itemstrack: null
                     });
                 });
             });
-        } else {
+        } else if (timestamp) {
             let resultArray = [];
             let cursor = db.collection('aircraft').find({timestamp: {$eq: timestamp}});
             cursor.forEach((doc) => {
@@ -135,16 +152,91 @@ app.get('/api/aircraft', (req, res) => {
                     itemsaircraftfiltered: JSON.stringify(resultArray),
                     items: null,
                     itemsaircraft: null,
-                    itemsfiltered: null
+                    itemsfiltered: null,
+                    itemstrack: null
                 });
+            });
+        } else {
+            res.render('index.html', {
+                itemsfiltered: null,
+                items: null,
+                itemsaircraft: null,
+                itemsaircraftfiltered: null,
+                itemstrack: null
             });
         }
     });
 });
 
+app.get('/api/tracks', (req, res) => {
+    let dbName = 'test';
+    transponder = req.query.transponder;
+    console.log(transponder);
+    MongoClient.connect(urlMongo, (err, client) => {
+        db = client.db(dbName);
+        if (transponder) {
+            const urlTrack = `https://opensky-network.org/api/tracks/all?icao24=${transponder}&time=0`;
+            request(urlTrack, (err, response, body) => {
+                if (err) {
+                    console.log('error:', error);
+                } else {
+                    console.log('BODY: ', body);
+                    itemtrack = {
+                        body: JSON.parse(body)
+                    };
+                    let i = 0;
+                    let maxElementTrack = 0;
+                    console.log('Path: ', itemtrack.body.path);
+                    itemtrack.body.path.forEach(() => {
+                        if (maxElementTrack < i) {
+                            maxElementTrack = maxElementTrack + 1;
+                        }
+                        i++;
+                    });
+                    console.log('maxElement1: ', maxElementTrack);
+                    let aircraftLatitude2 = itemtrack.body.path[maxElementTrack][1];
+                    let aircraftLongitude2 = itemtrack.body.path[maxElementTrack][2];
+                    console.log('aircraftLatitude2: ', aircraftLatitude2);
+                    console.log('aircraftLongitude2: ', aircraftLongitude2);
+                    let c = 0;
+                    let maxElementAircraft = 0;
+                    itemsaircaftrack.body.states.forEach(() => {
+                        console.log('c: ', c);
+                        if (itemsaircaftrack.body.states[c][0] === transponder) {
+                            maxElementAircraft = maxElementAircraft + c;
+                        }
+                        c++;
+                    });
+                    console.log('maxElement2: ', maxElementAircraft);
+                    let aircraftLatitude1 = itemsaircaftrack.body.states[maxElementAircraft][6];
+                    let aircraftLongitude1 = itemsaircaftrack.body.states[maxElementAircraft][5];
+                    console.log('aircraftLatitude1: ', aircraftLatitude1);
+                    console.log('aircraftLongitude1: ', aircraftLongitude1);
+                    let dis = (aircraftLatitude2 - aircraftLatitude1) * (aircraftLatitude2 - aircraftLatitude1) + (aircraftLongitude2 - aircraftLongitude1) * (aircraftLongitude2 - aircraftLongitude1);
+                    let distance = math.round(math.sqrt(dis) * 100);
+
+                    console.log('Distance: ', distance);
+                    res.render('index.html', {
+                        itemstrack: distance,
+                        items: null,
+                        itemsfiltered: null,
+                        itemsaircraft: null,
+                        itemsaircraftfiltered: null
+                    });
+                }
+            });
+        } else {
+            res.render('index.html', {
+                itemsfiltered: null,
+                items: null,
+                itemsaircraft: null,
+                itemsaircraftfiltered: null,
+                itemstrack: null
+            });
+        }
+    });
+
+});
+
 app.listen(3000, () => console.log('Listening on port 3000....'));
-
-
-
-
 
